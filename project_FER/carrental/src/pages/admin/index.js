d1fbda2a0b461a2bf7793a344424c9b625fed0e8
Tran Van Linh <vanbi12092004@gmail.com>
2025-07-21 11:24:10 +0700

    Force remove folder project_FER

--- project_FER/carrental/src/pages/admin/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AdminLayout } from "./AdminLayout";
-export { default as CarListAdmin } from "./CarListAdmin";
-export { default as AddCarPage } from "./AddCarPage";
-export { default as BookingListAdmin } from "./BookingListAdmin";
-export { default as AdminHomePage } from "./AdminHomePage";
